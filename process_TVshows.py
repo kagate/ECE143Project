5b4b3469294cdd31896e8ed10ecf9966d70e8fab
llbecerr1996 <llbecerr@ucsd.edu>
2021-03-03 16:28:32 -0800

    Updated tv show data

--- process_TVshows.py
+++ process_TVshows.py
@@ -1,85 +1,14 @@
-''' Consolidate TV show CSV files '''
-def TVshow_IMDB_clean_data(file_name='tv_shows.csv'):
-    ''' Remove TV shows without IMDB data '''
-    import pandas as pd
+''' 
+    This file contains functions which save a csv file (tv_shows.csv) with an added column under a new file name:
 
-    TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
-    TVshow_IMDB = pd.read_csv('title.basics.csv',dtype={'tconst':object, 'titleType':str, 'primaryTitle':str, 'originalTitle':str, 'isAdult':bool, 'startYear':object, 'endYear':object, 'runtimeMinutes':object, 'genres':object})
-
-    # generate dictionary of movies in tv_shows.csv with id numbers in IMDB data
-    TVshow_IMDB.drop(TVshow_IMDB[TVshow_IMDB['titleType'] != 'tvSeries'].index, inplace = True) # remove rows with title types other than tv series
-    TVshow_IMDB_clean = TVshow_IMDB[TVshow_IMDB['primaryTitle'].isin(TVshow_data.Title)] # keep only the rows with titles in tvshow_data
-    TVshow_IMDB_clean = TVshow_IMDB_clean.set_index('tconst') # set indices to be tconst
-    TVshow_id_dict = pd.Series(TVshow_IMDB_clean.index,index=TVshow_IMDB_clean.primaryTitle).to_dict() # generate dictionary of movies in tv_shows.csv and their id numbers from IMDB data
-
-    return TVshow_id_dict, TVshow_IMDB_clean, TVshow_data
-
-def TVshow_genres(file_name='tv_shows.csv'):
-    import pandas as pd
-    import numpy as np
-    from collections import Counter
-
-    # Other functions
-    import platform_plots
-
-    TVshow_id_dict, TVshow_IMDB_clean, TVshow_data = TVshow_IMDB_clean_data(file_name)
-    
-    # get genres for each movie in TVshow_id_dict
-    TVshow_data['Genres'] = '' # make column for genres
-    for index,row in TVshow_data.iterrows():
-        show = row[0]
-        if show in TVshow_id_dict: # if show title is in id dictionary
-            id_num = TVshow_id_dict[show] # get id number from dictionary
-            TVshow_data['Genres'][index] = TVshow_IMDB_clean.loc[id_num,'genres'] # put genre from IMDB data in Genres column of TVshow_data
-
-    TVshow_data.drop(TVshow_data[TVshow_data['Genres'] == ''].index, inplace=True) # remove rows without genres
-    TVshow_data.drop(TVshow_data[TVshow_data['Genres'] == '\\N'].index, inplace=True) # remove rows with genres '\\N'
-    TVshow_data = TVshow_data.reset_index()
-
-    # the following is just to plot using the old pie chart code
-    # NetflixGenresTemp_TVShow = [] # make empty array for TV show genres
-    # HuluGenresTemp_TVShow = [] # make empty array for TV show genres
-    # PrimeGenresTemp_TVShow = [] # make empty array for TV show genres
-    # DisneyGenresTemp_TVShow = [] # make empty array for TV show genres
-
-    # for index,row in TVshow_data.iterrows():
-    #     if row['Netflix']: # if the show is on Netflix platform
-    #         NetflixGenresTemp_TVShow.append(row['Genres'].split(','))
-    #     elif row['Hulu']:
-    #         HuluGenresTemp_TVShow.append(row['Genres'].split(','))
-    #     elif row['Prime Video']:
-    #         PrimeGenresTemp_TVShow.append(row['Genres'].split(','))
-    #     elif row['Disney+']:
-    #         DisneyGenresTemp_TVShow.append(row['Genres'].split(','))
-
-    # NetflixTVShowGenres = sum(NetflixGenresTemp_TVShow, [])
-    # HuluTVShowGenres = sum(HuluGenresTemp_TVShow, [])
-    # PrimeTVShowGenres = sum(PrimeGenresTemp_TVShow, [])
-    # DisneyTVShowGenres = sum(DisneyGenresTemp_TVShow, [])
-
-    # NetflixTVShowGenresDict = Counter(NetflixTVShowGenres)
-    # DictSize = len(NetflixTVShowGenresDict)
-    # ExplodeListNetflix_TVShow = [0.1]*DictSize
-    # platform_plots.pie_charts(NetflixTVShowGenresDict, "Netflix TV Shows Genres", "NetflixTVShowsGenres.png", ExplodeListNetflix_TVShow)
-
-    # HuluTVShowGenresDict = Counter(HuluTVShowGenres)
-    # DictSize = len(HuluTVShowGenresDict)
-    # ExplodeListHulu_TVShow = [0.1]*DictSize
-    # platform_plots.pie_charts(HuluTVShowGenresDict, "Hulu TV Shows Genres", "HuluTVShowsGenres.png", ExplodeListHulu_TVShow)
-
-    # PrimeTVShowGenresDict = Counter(PrimeTVShowGenres)
-    # DictSize = len(PrimeTVShowGenresDict)
-    # ExplodeListPrime_TVShow = [0.1]*DictSize
-    # platform_plots.pie_charts(PrimeTVShowGenresDict, "Prime Video TV Shows Genres", "PrimeTVShowsGenres.png", ExplodeListPrime_TVShow)
-
-    # DisneyTVShowGenresDict = Counter(DisneyTVShowGenres)
-    # DictSize = len(DisneyTVShowGenresDict)
-    # ExplodeListDisney_TVShow = [0.1]*DictSize
-    # platform_plots.pie_charts(DisneyTVShowGenresDict, "Disney+ TV Shows Genres", "DisneyTVShowsGenres.png", ExplodeListDisney_TVShow)
-
-    return TVshow_data
-
-def TVshow_year(file_name='tv_shows.csv'):
+    add_TVshow_decade outputs TV_shows_decade.csv --> original TV show csv with added decade column
+    add_TVshow_actor outputs TV_shows_actors.csv --> original TV show csv with added actors column (each entry has list of actors)
+    add_TVshow_producer outputs TV_shows_producers.csv --> original TV show csv with added producers column (each entry has list of executive producers)
+    add_TVshow_genre outputs TV_shows_genres.csv --> original TV show csv with added genres column (each entry has list of genres)
+    add_TVshow_language outputs TV_shows_languages.csv --> original TV show csv with added language column
+    consolidated_TVshow_data outputs TV_shows_all_features.csv --> original tv_shows.csv with all new columns generated in above functions
+'''
+def add_TVshow_decade(file_name='tv_shows.csv'):
     ''' Plot doughnut plot of tv show languages '''
 
     import pandas as pd
@@ -87,42 +16,139 @@ def TVshow_year(file_name='tv_shows.csv'):
 
     TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
 
-    TVshow_data['Decade'] = '' # make column for decade
-    for index,row in TVshow_data.iterrows():
-        TVshow_data['Decade'][index] = row['Year']//10*10 # calculate decade based on start year and add to decade column
+    TVshow_data['Decade'] = ''                                          # make column for decade
+    for index,row in TVshow_data.iterrows():                            # for every show in TVshow_data
+        TVshow_data['Decade'][index] = row['Year']//10*10               # calculate decade based on start year and add to decade column
 
-    # make Decade column categorical dtype
-    df_out_type = CategoricalDtype(categories=TVshow_data['Decade'].unique(), ordered=True)
-    TVshow_data['Decade'] = TVshow_data['Decade'].astype(df_out_type)   
+    TVshow_data.to_csv(r'TV_shows_decade.csv',index=False)              # save final dataframe to csv file
 
     return TVshow_data
 
-def TVshow_actor(file_name='tv_shows.csv'):
+def add_TVshow_actor(file_name='tv_shows.csv'):
     ''' Add directors and actors columns to TVshow dataframe '''
     import pandas as pd
-    
-    TVshow_people_info = pd.read_csv('name.basics.csv',dtype={'nconst':object, 'primaryName':str, 'birthYear':object, 'deathYear':object, 'primaryProfession':object, 'knownForTitles':object})
-    #TVshow_people_info = TVshow_people_info.set_index('nconst') # set indices to be tconst
-    TVshow_people = pd.read_csv('title.principals.csv',dtype={'tconst':object, 'ordering':int, 'nconst':object, 'category':object, 'job':object, 'characters':object})
-    TVshow_id_dict, TVshow_IMDB_clean, TVshow_data = TVshow_IMDB_clean_data(file_name)
+    from tvmaze.api import Api                                          # import TV show API with TV show attributes
 
-    TVshow_actors_clean = TVshow_people[TVshow_people['tconst'].isin(TVshow_IMDB_clean.index)] # only include people who are known for TV shows in TV show id number dictionary
-    TVshow_actors_clean = TVshow_actors_clean[TVshow_actors_clean['category'].notna()] # remove people who's profession is unknown
-    TVshow_actors_clean = TVshow_actors_clean.set_index('tconst') # set indices to tvshow id number
-    TVshow_actors = TVshow_actors_clean[TVshow_actors_clean['category'].isin(['actor','actress'])]
+    TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
+    api = Api()
 
-    # generate dictionary of people id number and name
-    TVshow_people_info = TVshow_people_info[TVshow_people_info['primaryProfession'].isin(['actor','actress'])]
-    people_id_dict = pd.Series(TVshow_people_info.primaryName,index=TVshow_people_info.index).to_dict() 
+    TVshow_data['Actors'] = ''                                          # make column for actors
+    for index,row in TVshow_data.iterrows():                            # for every show in TVshow_data
+        new_show = []                                                   
+        show_name = row[0]                                              # extract show title
+        try:
+            new_show.append(api.search.single_show(show_name))          # search for the show title in api
+        except:
+            new_show = []                                               # if an error is raised, return an empty array
+        if not new_show == [] and new_show[0].name == show_name:        # if a show was found with that title and the titles match exactly
+            id_num = api.search.single_show(show_name).id               # get the show id number in the api
+            actors = []
+            for actor in range(len(api.show.cast(id_num))):             # for all actors in the cast list
+                actor_name = api.show.cast(id_num)[actor].person.name   # get their name from their id number
+                actors.append(actor_name)                               # append name to output actor list
+            TVshow_data['Actors'][index] = actors                       # put output actor list in Actors column for this show
+        else:
+            TVshow_data['Actors'][index] = []                           # if no show is found, put an empty list in Actors column for this show
+    TVshow_data.to_csv(r'TV_shows_actors.csv',index=False)              # save final dataframe to csv file
 
-    TVshow_actors['TVshowTitle'] = '' # make column for TVshow title
-    TVshow_actors['ActorName'] = '' # make column for actor/actress name
-    for index,row in TVshow_actors.iterrows():
-        id_num = index # get id number from dictionary
-        name_id_num = row['nconst'] # get name id number 
-        TVshow_actors['TVshowTitle'][index] = TVshow_IMDB_clean.loc[id_num,'primaryTitle'] # put TV show title in TVshowTitle column
-        TVshow_actors['ActorName'][index] = people_id_dict[name_id_num] # put actor name in ActorName column
-    return TVshow_actors
+    return TVshow_data
+
+def add_TVshow_producer(file_name='tv_shows.csv'):
+    import pandas as pd
+    from tvmaze.api import Api # import TV show API with TV show attributes
+
+    TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
+    api = Api()
+
+    TVshow_data['Producers'] = ''                                       # make column for producers
+    for index,row in TVshow_data.iterrows():                            # for every show in TVshow_data
+        new_show = []
+        show_name = row[0]                                              # extract show title
+        try:
+            new_show.append(api.search.single_show(show_name))          # search for the show title in api
+        except:
+            new_show = []                                               # if an error is raised, return an empty array
+        if not new_show == [] and new_show[0].name == show_name:        # if a show was found with that title and the titles match exactly
+            id_num = api.search.single_show(show_name).id               # get the show id number in the api
+            crew_members = []
+            for crew_member in range(len(api.show.crew(id_num))):       # for all people in the crew list
+                crew_role = api.show.crew(id_num)[crew_member].type     # get the role of each person in the crew
+                if crew_role in ['Executive Producer','Co-Executive Producer']: # if their role is executive producer
+                    crew_members.append(api.show.crew(id_num)[crew_member].person.name) # append their name to the output producer list
+            TVshow_data['Producers'][index] = crew_members              # put output producer list in Producers column for this show
+            print(crew_members)
+        else:
+            TVshow_data['Producers'][index] = []                        # if no show is found, put an empty list in Producers column for this show
+    TVshow_data.to_csv(r'TV_shows_producers.csv',index=False)           # save final dataframe to csv file
+
+    return TVshow_data
+
+def add_TVshow_genre(file_name='tv_shows.csv'):
+    import pandas as pd
+    from tvmaze.api import Api
+
+    TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
+    api = Api()
+
+    TVshow_data['Genres'] = ''                                          # make column for genres
+    for index,row in TVshow_data.iterrows():                            # for every show in TVshow_data
+        new_show = []
+        show_name = row[0]                                              # extract show title
+        try:
+            new_show.append(api.search.single_show(show_name))          # search for the show title in api
+        except:
+            new_show = []                                               # if an error is raised, return an empty array
+        if not new_show == [] and new_show[0].name == show_name:        # if a show was found with that title and the titles match exactly
+            TVshow_data['Genres'][index] = new_show[0].genres           # add the genres list to the Genres column for that show
+        else:
+            TVshow_data['Genres'][index] = []                           # if no show is found, put an empty list in Genres column for this show
+    TVshow_data.to_csv(r'TV_shows_genres.csv',index=False)              # save final dataframe to csv file
+
+    return TVshow_data
+
+def add_TVshow_language(file_name='tv_shows.csv'):
+    import pandas as pd
+    from tvmaze.api import Api
+
+    TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
+    api = Api()
+
+    TVshow_data['Language'] = ''                                        # make column for languages
+    for index,row in TVshow_data.iterrows():                            # for every show in TVshow_data
+        new_show = []
+        show_name = row[0]                                              # extract show title
+        try:
+            new_show.append(api.search.single_show(show_name))          # search for the show title in api
+        except:
+            new_show = []                                               # if an error is raised, return an empty array
+        if not new_show == [] and new_show[0].name == show_name:        # if a show was found with that title and the titles match exactly
+            TVshow_data['Language'][index] = new_show[0].language       # add the language to the Language column for that show
+        else:
+            TVshow_data['Language'][index] = []                         # if no show is found, put an empty list in Language column for this show
+    TVshow_data.to_csv(r'TV_shows_languages.csv',index=False)           # save final dataframe to csv file
+
+    return TVshow_data
+
+def consolidated_TVshow_data(file_name='tv_shows.csv'):
+    import pandas as pd
+
+    # import all modified csv's from functions above
+    TVshow_data = pd.read_csv(file_name,index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool})
+    TVshow_data_decade = pd.read_csv('TV_shows_decade.csv',index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool, 'Decade':object})
+    TVshow_data_actors = pd.read_csv('TV_shows_actors.csv',index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool, 'Actors':object})
+    TVshow_data_producers = pd.read_csv('TV_shows_producers.csv',index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool, 'Producers':object})
+    TVshow_data_genres = pd.read_csv('TV_shows_genres.csv',index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool, 'Genres':object})
+    TVshow_data_languages = pd.read_csv('TV_shows_languages.csv',index_col=0,dtype={'Title': str, 'Year': int, 'Age': object, 'IMDb':float, 'Rotten Tomatoes':object, 'Netflix':bool, 'Hulu':bool, 'Prime Video':bool, 'Disney+':bool, 'type':bool, 'Language':object})
+
+    # append every new column in each of the above csv's to the original TVshow_data csv
+    TVshow_data['Decade'] = TVshow_data_decade['Decade']
+    TVshow_data['Actors'] = TVshow_data_actors['Actors']
+    TVshow_data['Producers'] = TVshow_data_producers['Producers']
+    TVshow_data['Genres'] = TVshow_data_genres['Genres']
+    TVshow_data['Language'] = TVshow_data_languages['Language']
+
+    # save final dataframe to csv file
+    TVshow_data.to_csv(r'TV_shows_all_features.csv',index=False)           
 
 if __name__ == '__main__':
-    print(TVshow_actor(file_name='tv_shows.csv'))
+    print(add_TVshow_producer(file_name='tv_shows.csv'))
